d0e1f9395573627628c68d384edaacf8c10668bd
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-01 09:53:58 -0700

    save and connect the uploaded file

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import os
 
 st.title("SQLite Table Viewer")
 uploaded_file = st.file_uploader("Choose a .db file", type="db")
+
+if uploaded_file is not None:
+    db_file_path = uploaded_file.name
+    with open(db_file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    conn = sqlite3.connect(db_file_path)
+    cursor = conn.cursor()
